3ea7138b752e545116502f7f9b45b94d072920c5
catnipan <yifan@catnipan.com>
2024-09-19 23:53:14 -0400

    init commit wip

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>memorize solfege</h1>
+      <h2>// wip</h2>
+      <div>
+        built with ❤️ by <a href="https://catnipan.com">@catnipan</a>
+      </div>
+    </>
+  );
+}
+
+export default App;
